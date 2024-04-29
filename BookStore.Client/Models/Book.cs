5efd76c3c9cbd9e6560bf44c7c17314da92ed2e2
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-04-29 16:09:42 +0200

    validation

--- BookStore.Client/Models/Book.cs
+++ BookStore.Client/Models/Book.cs
@@ -1,11 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BookStore.Client.Models;
 
 public class Book
 {
     public int Id { get; set; }
+
+    [Required]
+    [StringLength(50)]
     public required string Name { get; set; }
+
+    [Required]
+    [StringLength(20)]
     public required string Genre { get; set; }
+
+    [Required]
+    [StringLength(50)]
     public required string Author { get; set; }
+
+    [Range(1, 100)]
     public decimal Price { get; set; }
     public DateTime ReleaseDate { get; set; }
 }
